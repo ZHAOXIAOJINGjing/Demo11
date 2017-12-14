1bf670d849fa27f4d14eefae7e0d4c35f2bbd3e3
1983164263@qq.com <1983164263@qq.com>
2017-12-14 17:23:08 +0800

    Signed-off-by: 1983164263@qq.com <1983164263@qq.com>

--- src/A.java
+++ src/A.java
@@ -2,7 +2,7 @@
 public class A {
 	public static void main(String[] args) {
 		System.out.println("dsdsd");
-		
+				System.out.println("dsdsd");		System.out.println("dsdsd");		System.out.println("dsdsd");		System.out.println("dsdsd");		System.out.println("dsdsd");
 		
 	}
 
